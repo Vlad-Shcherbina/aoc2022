56cc1e047fb33600d2e0b75b19d485587e760188
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-12-06 23:42:37 +0100

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,8 +1,10 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let part1 = input.split("\n\n")
+    let mut elves: Vec<i32> = input.split("\n\n")
         .map(|elf|
             elf.split_terminator('\n').map(
                 |s| s.parse::<i32>().unwrap()).sum::<i32>())
-        .max().unwrap();
-    out(part1.to_string());
+        .collect();
+    elves.sort();
+    out(elves.last().unwrap().to_string());
+    out(elves[elves.len() - 3..].iter().sum::<i32>().to_string());
 }
