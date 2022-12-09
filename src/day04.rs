536186d5a2beb8e9871e350f50829d801e1fdb6c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-12-09 11:17:44 +0100

    Day 4, part 2

--- src/day04.rs
+++ src/day04.rs
@@ -4,15 +4,20 @@ fn parse_range(s: &str) -> (i32, i32) {
 }
 
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let mut overlaps = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in input.split_terminator('\n') {
         let (first, second) = line.split_once(',').unwrap();
         let first = parse_range(first);
         let second = parse_range(second);
         if first.0 <= second.0 && first.1 >= second.1 ||
            first.0 >= second.0 && first.1 <= second.1 {
-            overlaps += 1;
+            part1 += 1;
+        }
+        if first.0.max(second.0) <= first.1.min(second.1) {
+            part2 += 1;
         }
     }
-    out(overlaps.to_string());
+    out(part1.to_string());
+    out(part2.to_string());
 }
