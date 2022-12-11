0919fce18d0c2b030f9269464ac20d726d9a5e72
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-12-11 19:36:30 +0100

    Day 6, part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,13 +1,15 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let input = input.trim_end().as_bytes();
-    let mut i = 4;
-    let result = loop {
-        if all_distinct(&input[i - 4 .. i]) {
-            break i;
-        }
-        i += 1;
-    };
-    out(result.to_string());
+    for n in [4, 14] {
+        let mut i = n;
+        let result = loop {
+            if all_distinct(&input[i - n .. i]) {
+                break i;
+            }
+            i += 1;
+        };
+        out(result.to_string());
+    }
 }
 
 fn all_distinct(xs: &[u8]) -> bool {
