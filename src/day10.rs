23be8277af175ed6c1e8e5ec672eddc9014e89a8
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-12-13 22:20:00 +0100

    Day 10, part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,24 +1,37 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let ts = [20, 60, 100, 140, 180, 220];
     let mut t = 1;
-    let mut x = 1;
+    let mut x: i32 = 1;
     let mut res = 0;
+    let mut screen = String::new();
     for line in input.split_terminator('\n') {
+        let dt;
+        let dx: i32;
         if line == "noop" {
+            dt = 1;
+            dx = 0;
+        } else if let Some(arg) = line.strip_prefix("addx ") {
+            dt = 2;
+            dx = arg.parse().unwrap();
+        } else {
+            panic!();
+        }
+        for _ in 0..dt {
+            if t % 40 == 1 {
+                screen.push('\n');
+            }
             if ts.contains(&t) {
                 res += t * x;
             }
-            t += 1;
-        } else if let Some(arg) = line.strip_prefix("addx ") {
-            let arg: i32 = arg.parse().unwrap();
-            for _ in 0..2 {
-                if ts.contains(&t) {
-                    res += t * x;
-                }
-                t += 1;
+            if ((t - 1) % 40 - x).abs() <= 1 {
+                screen.push('#');
+            } else {
+                screen.push('.');
             }
-            x += arg;
+            t += 1;
         }
+        x += dx;
     }
     out(res.to_string());
+    out(screen);
 }
